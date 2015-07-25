667b471a19a155c152fb69b571faf0aa6ffd6dae
Benjamin Stegeman <benstegeman@escort.cis.ksu.edu>
2015-07-25 17:38:17 -0500

    first commit, lists comments for hard-coded flair in plain text

---
+++ popup.js
@@ -0,0 +1,121 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabJsonUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url.slice(0, tab.url.length - 1)+".json";
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.json should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, function(tabs) {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+
+
+};
+
+/**
+ * @param {string} searchTerm - Search term for Google Image search.
+ * @param {function(string,number,number)} callback - Called when an image has
+ *   been found. The callback gets the URL, width and height of the image.
+ * @param {function(string)} errorCallback - Called when the image is not found.
+ *   The callback gets a string that describes the failure reason.
+ */
+
+function getComments(url, callback, errorCallback) {
+  
+  var x = new XMLHttpRequest();
+  
+  x.open('GET', url);
+  x.responseType = 'json';
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    if (!response) {
+      errorCallback('No response from Reddit!');
+      return;
+    }
+    var comments = response[1].data.children;
+    
+    callback(comments);
+  };
+  x.onerror = function() {
+    errorCallback('Network error.');
+  };
+  x.send();
+}
+
+function getCommentsByFlair(flair, comments, callback) {
+	filtered_comments = [];
+
+	//iterate through all comments and capture the ones with the sought-after flair
+	comments.forEach(function(comment, i, comments) {
+		if (comment.data.author_flair_text != null) {
+			if (comment.data.author_flair_text.toUpperCase() == flair.toUpperCase()){
+				console.log(comment.data.body);
+				filtered_comments.push(comment.data.body);
+			}
+		}
+	});
+	if (filtered_comments.length == 0) {
+		renderComments('Couldn\'t find any '+flair+' comments. Sorry!');
+		return;
+	}
+	callback(filtered_comments);
+}
+
+	
+function renderComments(statusText) {
+  document.getElementById('single-comment').innerHTML += "<p>"+statusText+"</p>";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabJsonUrl(function(url) {  
+    flair = "chiefs";
+    getComments(url, function(comments) {
+    	getCommentsByFlair(flair, comments, function(filtered_comments) {
+    		for (var i = 0; i < 3; i++) {
+			renderComments(filtered_comments[i]+"\n");
+		}
+      	}),
+    	function(errorMessage) {
+    		renderComments('Cannot display comments. ' + errorMessage);
+    	};
+    });
+    });
+});
